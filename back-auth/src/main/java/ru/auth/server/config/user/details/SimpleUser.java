942aefc1adbacfe62092939ee4ae77c9c7ffca73
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-04 21:35:17 +0400

    Part 2 Listing 3.8 Defining the JPA User entity class add lombok hibernate

---
+++ back-auth/src/main/java/ru/auth/server/config/user/details/SimpleUser.java
@@ -0,0 +1,65 @@
+package ru.auth.server.config.user.details;
+
+import org.apache.logging.log4j.util.Base64Util;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.apache.logging.log4j.util.Base64Util.encode;
+
+public class SimpleUser implements UserDetails {
+    private final String username;
+    private final String password;
+
+    public SimpleUser(String username, String password) {
+        User.UserBuilder builder1 = User.withUsername("bill");
+        UserDetails u1 = builder1
+                .password("12345")
+                .authorities("read", "write")
+                .passwordEncoder(Base64Util::encode)
+                .accountExpired(false)
+                .disabled(true)
+                .build();
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+// Omitted code
+}
---
+++ back-auth/src/main/java/ru/auth/server/entity/User.java
@@ -0,0 +1,17 @@
+package ru.auth.server.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String authority;
+}
