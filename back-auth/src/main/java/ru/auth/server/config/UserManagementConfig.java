6faf2c1adc5cf4999a8ed57d78982484909fab80
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-05 20:39:42 +0400

    Part 2 3.3.3 Implementing the UserDetailsManager contract

--- back-auth/src/main/java/ru/auth/server/config/UserManagementConfig.java
+++ back-auth/src/main/java/ru/auth/server/config/UserManagementConfig.java
@@ -2,21 +2,26 @@ package ru.auth.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.auth.server.config.user.details.InMemoryUserDetailsService;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class UserManagementConfig {
+
     @Bean
-    UserDetailsService userDetailsService() {
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService userDetailsService() {
+        UserDetails u = new User("john", "12345", Collections.singletonList(new SimpleGrantedAuthority ("read")));
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
