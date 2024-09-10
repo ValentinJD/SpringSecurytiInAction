521f1bcf7c612507ba7d641a3a334c63e627e025
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-10 22:09:25 +0400

    Part 2 Chapter 4 Encryptors

--- back-auth/src/main/java/ru/auth/server/controllers/HelloController.java
+++ back-auth/src/main/java/ru/auth/server/controllers/HelloController.java
@@ -2,6 +2,10 @@ package ru.auth.server.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +27,30 @@ public class HelloController {
                 "encode scrypt: 12345 is:  " + scryptPasswordEncoder.encode("{scrypt}12345") + "\n" +
                 "encode sha256: 12345 is:  " + sha512PasswordEncoder.encode("{sha256}12345") + "\n" +
                 "encode plaintText: 12345 is:  " + NoOpPasswordEncoder.getInstance().encode("{noop}12345") ;
+    }
+
+    @GetMapping("/generateKey")
+    public String generateKey() {
+        BytesKeyGenerator keyGenerator1 = KeyGenerators.secureRandom();
+        byte [] key = keyGenerator1.generateKey();
+        int keyLength = keyGenerator1.getKeyLength();
+        BytesKeyGenerator keyGenerator2 = KeyGenerators.secureRandom(16);
+        BytesKeyGenerator keyGenerator = KeyGenerators.shared(16);
+        byte [] key1 = keyGenerator.generateKey();
+        byte [] key2 = keyGenerator.generateKey();
+        return "key1: " + new String(key1) + "\n" +
+                "key2: " + new String(key2) + "\n";
+    }
+
+    @GetMapping("/encrypt")
+    public String encrypt() {
+        String salt = KeyGenerators.string().generateKey();
+        String password = "secret";
+        String valueToEncrypt = "HELLO";
+        BytesEncryptor e = Encryptors.standard(password, salt);
+        byte [] encrypted = e.encrypt(valueToEncrypt.getBytes());
+        byte [] decrypted = e.decrypt(encrypted);
+        return "encrypted: " + new String(encrypted) + "\n" +
+                "decrypted: " + new String(decrypted) + "\n";
     }
 }
