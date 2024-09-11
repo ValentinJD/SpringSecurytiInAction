9c114dbca8c9844b1e2a1335c41fdd6a8d91694c
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-11 08:26:27 +0400

    Part 2 Chapter 5 Filters Исключили UserDetailsServiceAutoConfiguration

--- back-auth/src/main/java/ru/auth/server/BackendAuthenticationApplication.java
+++ back-auth/src/main/java/ru/auth/server/BackendAuthenticationApplication.java
@@ -3,8 +3,9 @@ package ru.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BackendAuthenticationApplication {
 
     public static void main(String[] args) {
--- back-auth/src/main/java/ru/auth/server/config/filters/AuthenticationLoggingFilter.java
+++ back-auth/src/main/java/ru/auth/server/config/filters/AuthenticationLoggingFilter.java
@@ -2,19 +2,22 @@ package ru.auth.server.config.filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        var httpRequest = (HttpServletRequest) servletRequest;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var httpRequest = (HttpServletRequest) request;
         var requestId = httpRequest.getHeader("Request-Id");
         log.info("Successfully authenticated request with id {}", requestId);
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
 }
