9989ef3e565503b49d6dac87549ac6f9ef97bc4f
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-26 21:40:11 +0400

    Part 3 Chapter 10 CORS

--- back-auth/src/main/java/ru/auth/server/config/ProjectConfig.java
+++ back-auth/src/main/java/ru/auth/server/config/ProjectConfig.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,34 +33,13 @@ public class ProjectConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider);
 
-//        Or you can similarly use a regex matcher:
+        http.csrf(AbstractHttpConfigurer::disable);
 
-//        http.httpBasic(Customizer.withDefaults());
-        http.csrf(c -> {
-            c.csrfTokenRepository(customTokenRepository);
-            c.csrfTokenRequestHandler(
-                    new CsrfTokenRequestAttributeHandler()
-            );
-            HandlerMappingIntrospector i = new HandlerMappingIntrospector();
-//            MvcRequestMatcher r = new MvcRequestMatcher(i, "/ciao");
-//            c.ignoringRequestMatchers(r);
-            String pattern = "\\/console\\/\\w+.\\w+\\D\\D+\\w+";
-            String httpMethod = HttpMethod.POST.name();
-            RegexRequestMatcher r2 = new RegexRequestMatcher(pattern, httpMethod);
-            c.ignoringRequestMatchers(r2);
-        });
-//        http.formLogin(Customizer.withDefaults());
         http.authorizeHttpRequests(
                 c -> c.requestMatchers("/console/**").permitAll()
                         .anyRequest().permitAll());
         http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
-        SecurityContextRepository securityContextRepository = new RequestAttributeSecurityContextRepository();
-        SecurityContextHolderFilter securityContextHolderFilter =
-                new SecurityContextHolderFilter(securityContextRepository);
-        securityContextHolderFilter.setSecurityContextHolderStrategy(SecurityContextHolder.getContextHolderStrategy());
-        http.addFilter(securityContextHolderFilter);
         return http.build();
     }
 
--- back-auth/src/main/java/ru/auth/server/controllers/MainController.java
+++ back-auth/src/main/java/ru/auth/server/controllers/MainController.java
@@ -1,13 +1,24 @@
 package ru.auth.server.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class MainController {
 
-    @GetMapping("/main")
+    @GetMapping("/")
     public String main() {
         return "main.html";
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    public String test() {
+        log.info("Test method called");
+        return "HELLO";
     }
 }
