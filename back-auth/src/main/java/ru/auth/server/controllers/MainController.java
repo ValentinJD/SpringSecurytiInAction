9d03347cb9c6a8a34388a865f2197ee02d200fe5
Valentin Kudinov <moysait1984@yandex.ru>
2024-09-26 21:49:16 +0400

    Part 3 Chapter 10 CORS @CrossOrigin("http://localhost:8080")

--- back-auth/src/main/java/ru/auth/server/controllers/MainController.java
+++ back-auth/src/main/java/ru/auth/server/controllers/MainController.java
@@ -2,6 +2,7 @@ package ru.auth.server.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,7 @@ public class MainController {
 
     @PostMapping("/test")
     @ResponseBody
+    @CrossOrigin("http://localhost:8080")
     public String test() {
         log.info("Test method called");
         return "HELLO";
