ea198be73846489d5c93ee0317d1a089c1facc15
Valentin Kudinov <moysait1984@yandex.ru>
2024-10-01 21:46:01 +0400

    Part 3 Chapter 11 @PreAuthorize

--- back-auth/src/main/java/ru/auth/server/config/ProjectConfig.java
+++ back-auth/src/main/java/ru/auth/server/config/ProjectConfig.java
@@ -3,7 +3,6 @@ package ru.auth.server.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -11,20 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
-import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import ru.auth.server.repository.CustomCsrfTokenRepository;
 
-import java.util.List;
 
 @EnableMethodSecurity
 @EnableAsync
--- back-auth/src/main/java/ru/auth/server/controllers/HelloController.java
+++ back-auth/src/main/java/ru/auth/server/controllers/HelloController.java
@@ -2,12 +2,12 @@ package ru.auth.server.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.auth.server.service.NameService;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,9 +16,9 @@ public class HelloController {
 
     private final NameService nameService;
 
-    @GetMapping("/hello")
-    public String getHello() {
-        return "Hello, " + nameService.getName();
+    @GetMapping("/secret/names/{name}")
+    public List<String> getHello(@PathVariable("name") String name) {
+        return nameService.getSecretNames(name);
     }
 
 
