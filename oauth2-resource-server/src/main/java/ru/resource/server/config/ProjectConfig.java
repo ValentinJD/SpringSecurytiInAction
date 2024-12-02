161abc2144a0e8b50b78afa3288f90c264f00839
Valentin Kudinov <moysait1984@yandex.ru>
2024-12-02 21:36:10 +0400

    Part 4 Chapter 15 oauth2-resource-server

--- oauth2-resource-server/src/main/java/ru/resource/server/config/ProjectConfig.java
+++ oauth2-resource-server/src/main/java/ru/resource/server/config/ProjectConfig.java
@@ -1,9 +1,12 @@
 package ru.resource.server.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,17 +21,20 @@ public class ProjectConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer(
-                c -> c.opaqueToken(
-                        o -> o.introspectionUri(introspectionUri)
-                                .introspectionClientCredentials(
-                                        resourceServerClientID,
-                                        resourceServerSecret)
-                )
+                j -> j.authenticationManagerResolver(
+                        authenticationManagerResolver())
         );
         http.authorizeHttpRequests(
                 c -> c.anyRequest().authenticated()
         );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+        return new JwtIssuerAuthenticationManagerResolver(
+                "http://localhost:7070",
+                "http://localhost:8080");
     }
 
 }
