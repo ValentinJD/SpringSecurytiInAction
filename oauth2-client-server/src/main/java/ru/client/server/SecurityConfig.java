32a014c4e03cb7fec407451a00c797a668903238
Valentin Kudinov <moysait1984@yandex.ru>
2024-12-04 21:51:14 +0400

    Part 4 Chapter 16.1.4 client

--- oauth2-client-server/src/main/java/ru/client/server/SecurityConfig.java
+++ oauth2-client-server/src/main/java/ru/client/server/SecurityConfig.java
@@ -1,20 +1,44 @@
 package ru.client.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @ComponentScan
 @Configuration
 public class SecurityConfig {
+
+    @Value("${client-id}")
+    private String clientId;
+    @Value("${client-secret}")
+    private String clientSecret;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.oauth2Login(Customizer.withDefaults());
         http.authorizeHttpRequests(c -> c.anyRequest()
                 .authenticated());
         return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                this.googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
     }
 }
