f37b0a09ef4923d7e13f1a63e4fc5c01dba4b9f3
Valentin Kudinov <moysait1984@yandex.ru>
2024-12-09 21:11:32 +0400

    Part 4 Chapter 16.1.5 Config and save clients in ClientRegistrationRepository

--- oauth2-client-server/src/main/java/ru/client/server/HomeController.java
+++ oauth2-client-server/src/main/java/ru/client/server/HomeController.java
@@ -2,13 +2,22 @@ package ru.client.server;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+//    @GetMapping("/")
+//    public Resource home() {
+//        return new ClassPathResource("templates/index.html");
+//    }
+
     @GetMapping("/")
-    public Resource home() {
+    public Resource home(OAuth2AuthenticationToken authentication) {
+        OAuth2User principal = authentication.getPrincipal();
+// do something with the authentication
         return new ClassPathResource("templates/index.html");
     }
 }
--- oauth2-client-server/src/main/java/ru/client/server/SecurityConfig.java
+++ oauth2-client-server/src/main/java/ru/client/server/SecurityConfig.java
@@ -10,7 +10,14 @@ import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @ComponentScan
 @Configuration
@@ -31,14 +38,34 @@ public class SecurityConfig {
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(
-                this.googleClientRegistration());
+        List<ClientRegistration> clientRegistrations = new ArrayList<>();
+        clientRegistrations.add(googleClientRegistration());
+        clientRegistrations.add(clientRegistration());
+        return new InMemoryClientRegistrationRepository(clientRegistrations);
     }
 
     private ClientRegistration googleClientRegistration() {
         return CommonOAuth2Provider.GOOGLE.getBuilder("google")
                 .clientId(clientId)
                 .clientSecret(clientSecret)
+                .build();
+    }
+
+    private ClientRegistration clientRegistration() {
+        return ClientRegistration.withRegistrationId("client")
+                .clientId("client")
+                .clientSecret("secret")
+                .clientName("Custom")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope(OidcScopes.OPENID)
+                .tokenUri("http://127.0.0.1:8080/oauth2/token")
+                .authorizationUri("http://127.0.0.1:8080/oauth2/authorize")
+                .jwkSetUri("http://127.0.0.1:8080/oauth2/jwks")
+                .userInfoUri("http://127.0.0.1:8080/userinfo")
+                .issuerUri("http://127.0.0.1:8080")
+                .redirectUri("http://localhost:7070/login/oauth2/code/my_authorization_server")
+//                .providerConfigurationMetadata(Map.of("provider", "my_authorization_server"))
                 .build();
     }
 }
