333583e363df61b9b8c8ad38bd64368c1246599a
Valentin Kudinov <moysait1984@yandex.ru>
2024-12-13 15:51:51 +0400

    Part 5 17.6 pathMatchers

--- reactive-app/src/main/java/ru/reactive/app/config/ProjectConfig.java
+++ reactive-app/src/main/java/ru/reactive/app/config/ProjectConfig.java
@@ -2,11 +2,15 @@ package ru.reactive.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -24,5 +28,17 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeExchange(
+                c -> c.pathMatchers(HttpMethod.GET, "/hello")
+                        .authenticated()
+                        .anyExchange()
+                        .permitAll()
+        );
+        return http.build();
     }
 }
