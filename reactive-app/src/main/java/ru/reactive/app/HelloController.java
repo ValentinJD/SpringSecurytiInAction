826b7e1832316771f62f7f618db26bcaa3b5975d
Valentin Kudinov <moysait1984@yandex.ru>
2024-12-13 15:31:52 +0400

    Part 5 17.2 Mono<Authentication> auth

--- reactive-app/src/main/java/ru/reactive/app/HelloController.java
+++ reactive-app/src/main/java/ru/reactive/app/HelloController.java
@@ -1,5 +1,6 @@
 package ru.reactive.app;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,7 +8,8 @@ import reactor.core.publisher.Mono;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("Hello!");
+    public Mono<String> hello(Mono<Authentication> auth) {
+        Mono<String> message = auth.map(a -> "Hello " + a.getName());
+        return message;
     }
 }
